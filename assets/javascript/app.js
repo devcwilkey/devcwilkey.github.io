161221aa1963a9afcec5cbfad96c3d3603c011b1
devcwilkey <DevCWilkey@gmail.com>
2019-07-29 15:50:32 -0700

    Adding the Basic Updates for the Project Homework

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+// Make this available via That
+var that = this;
+
+
+// Same thing as $(documnet).ready(function(){}); ----- Shorthand version $(function(){})
+$(function(){
+    console.log(this)
+});
